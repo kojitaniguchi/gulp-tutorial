0f39eda579aa9098ba598a54753cc80c7e0ead22
kojitaniguchi <tanigu0511@gmail.com>
2017-09-07 00:03:38 +0900

    gulp_webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+//module.exports = {
+//  entry: "./src/main.js",
+//  output: {
+//    filename: "bundle.js"
+//  }
+//}
+
+const path = require('path');
+
+module.exports = {
+    entry: {
+        bundle: './src/app.js'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                test: /\.js[x]?$/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    }
+};
